65d8e61a51672e935ba3228affb1ac7fde5c155e
sym4545 <576446974@qq.com>
2017-12-04 18:11:06 +0800

    孙义明提交Struts2 T6上机作业

---
+++ Struts2/T6/25孙义明/T6/src/com/ming/t6/action/LocalAction.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.ming.t6.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @author jayli
+ *
+ */
+public class LocalAction extends ActionSupport {
+
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return SUCCESS;
+	}
+
+}
---
+++ Struts2/T6/25孙义明/T6/src/com/ming/t6/action/UserAction.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.ming.t6.action;
+
+import com.ming.t6.bean.User;
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * @author jayli
+ *
+ */
+public class UserAction extends ActionSupport {
+	private User user;
+	
+	
+	@Override
+	public void validate() {
+		if ("".equals(user.getUname())) {
+			addFieldError("user.uname", getText("uname.required"));
+		}
+		if ("".equals(user.getUpwd())) {
+			addFieldError("user.upwd", getText("upwd.required"));
+		}
+	}
+	
+	public String login() {
+		return SUCCESS;
+	}
+	
+	public String execute() {
+		return SUCCESS;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+}

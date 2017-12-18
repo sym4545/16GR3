25b5063a8c16aedf58d0842f72f929a27c072a89
sym4545 <576446974@qq.com>
2017-12-18 13:30:24 +0800

    1，孙义明提交T4作业

---
+++ Hibernate/T4/25孙义明/上机作业/HBM_T4/src/com/qhit/lh/gr3/ming/service/impl/BaseServiceImpl.java
@@ -0,0 +1,47 @@
+package com.qhit.lh.gr3.ming.service.impl;
+
+import java.util.List;
+
+import com.qhit.lh.gr3.ming.dao.BaseDao;
+import com.qhit.lh.gr3.ming.dao.impl.BaseDaoImpl;
+import com.qhit.lh.gr3.ming.service.BaseService;
+
+/**
+ * @author admin
+ * 2017年12月11日
+ */
+public class BaseServiceImpl implements BaseService {
+	private BaseDao baseDao = new BaseDaoImpl();
+
+	@Override
+	public void add(Object obj) {
+		// TODO Auto-generated method stub
+		baseDao.add(obj);
+	}
+
+	@Override
+	public void del(Object obj) {
+		// TODO Auto-generated method stub
+		baseDao.del(obj);
+	}
+
+	@Override
+	public void update(Object obj) {
+		// TODO Auto-generated method stub
+		baseDao.update(obj);
+	}
+
+	@Override
+	public List<Object> getAll(String fromObject) {
+		// TODO Auto-generated method stub
+		return baseDao.getAll(fromObject);
+	}
+
+	@Override
+	public Object getObjectById(Class clazz, int id) {
+		// TODO Auto-generated method stub
+		return baseDao.getObjectById(clazz, id);
+	}
+
+
+}
---
+++ Hibernate/T4/25孙义明/上机作业/HBM_T4/test/com/qhit/lh/gr3/Test.java
@@ -0,0 +1,29 @@
+package com.qhit.lh.gr3;
+
+import static org.junit.Assert.*;
+
+import com.qhit.lh.gr3.ming.bean.Dept;
+import com.qhit.lh.gr3.ming.bean.Emp;
+import com.qhit.lh.gr3.ming.bean.User;
+import com.qhit.lh.gr3.ming.service.BaseService;
+import com.qhit.lh.gr3.ming.service.impl.BaseServiceImpl;
+
+public class Test {
+	BaseService bs = new 	BaseServiceImpl();
+	@org.junit.Test
+	public void add(){
+		Emp emp1 = new Emp();
+		emp1.setEname("11111111");
+		emp1.setSex("女");
+		Emp emp2 = new Emp();
+		emp2.setEname("22222222");
+		emp2.setSex("女");
+		//dept
+		Dept dept = (Dept) bs.getObjectById(Dept.class, 1);
+		//多对一
+		dept.getEmps().add(emp1);
+		dept.getEmps().add(emp2);
+		bs.add(dept);
+	}
+
+}
